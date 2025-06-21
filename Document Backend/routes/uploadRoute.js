1185401ecdcadc219ebba01197f66fe0919a4dbb
pawansct <pawansct@gmail.com>
2025-06-21 22:02:46 +0530

    Delete Document Backend directory

--- Document Backend/routes/uploadRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../middleware/multer'); // Multer middleware
-const handleFileUpload  = require('../apis/fileupload');
-
-router.post('/upload', upload.single('file'), handleFileUpload);
-
-module.exports = router;
